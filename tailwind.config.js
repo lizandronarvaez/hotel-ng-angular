4265cafa960e150b03bb1de1e7354d0dab187329
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-11-24 00:37:50 +0100

    styles: estilos personalizados de los componentes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ export default {
   ],
   theme: {
     extend: {},
+    backgroundColor: {
+      primary: "#f3f7ff",
+      secondary:"#924dff",
+      secondaryHover:"#7440c7",
+    },
+    textColor: {
+      primary: "#696969",
+      primaryHover: "#924dff",
+      blackColor: "#2f3045",
+    },
+    fontSize: {
+      primary: "16px",
+      secondary:"20px"
+    }
   },
   plugins: [],
 }
