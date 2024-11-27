50de9d2327a94a04a0e3d2c782b294f9b29e5282
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-11-27 19:23:43 +0100

    Feat: Agregando un nuevo componente que servira para consultar las reservas

---
+++ src/app/components/pages/reservation/reservation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation',
+  standalone: true,
+  imports: [],
+  templateUrl: './reservation.component.html',
+})
+export class ReservationComponent {
+
+}
