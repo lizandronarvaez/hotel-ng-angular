f16919e2da4a5b138203208bf1f9c15513fc9ce3
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-11-27 19:28:14 +0100

    Feat: Agregar componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/common/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
+import { HotelListsComponent } from './components/pages/hotel-lists/hotel-lists.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hotelBooks';
