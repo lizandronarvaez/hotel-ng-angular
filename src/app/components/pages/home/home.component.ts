0d1f8f91633f87916f54aed09c00fa554946ddf3
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-11-27 19:30:23 +0100

    Feat: Agregar componente Home

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { DpDatePickerModule, IDatePickerConfig } from 'ng2-date-picker';
 import dayjs from 'dayjs';
 import 'dayjs/locale/es';
+import { RouterModule } from '@angular/router';
+import { HotelListsComponent } from '../hotel-lists/hotel-lists.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [DpDatePickerModule],
+  imports: [DpDatePickerModule, RouterModule, HotelListsComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-
   datePickerConfig: IDatePickerConfig = {
     format: 'DD-MM-YYYY',
     firstDayOfWeek: 'mo',
