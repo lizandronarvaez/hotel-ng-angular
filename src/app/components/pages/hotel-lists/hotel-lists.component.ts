eb74df1e35efa9725b173156b5e734e36f55d20f
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-11-27 19:22:29 +0100

    feat: Agregando estructura html sin funcionalidad

---
+++ src/app/components/pages/hotel-lists/hotel-lists.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-hotel-lists',
+  standalone: true,
+  imports: [],
+  templateUrl: './hotel-lists.component.html',
+
+})
+export class HotelListsComponent {}
