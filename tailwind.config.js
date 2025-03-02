41f4cf8616709a66d8e506309b4753c5dbade9c2
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-02 15:50:01 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-    backgroundColor: {
-      primary: "#f3f7ff",
-      secondary:"#924dff",
-      secondaryHover:"#7440c7",
+    content: [
+        "./index.html",
+        "./src/**/*.{html,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "home-bg": "url('https://www.kayak.es/rimg/himg/b3/10/90/ice-178163-120191068-424668.jpg?width=1366&height=768&crop=true')",
+                "bookingImage": "url('https://pro-static.h10hotels.com/gallery/CabeceraH10NOV2.jpg')"
+            },
+            backgroundColor: {
+                primaryBg: "#f3f7ff",
+                secondaryBg: "#924dff",
+                secondaryHoverBg: "#7440c7",
+                whiteBg: "#ffffff",
+                white2: "#f9f9f9"
+            },
+            textColor: {
+                primaryC: "#696969",
+                primaryHoverC: "#924dff",
+                blackColorC: "#2f3045",
+                whiteColorC: "#ffffff"
+            },
+            fontSize: {
+                primaryF: "16px",
+                secondaryF: "20px",
+                paragrapF: "24px",
+                titleF: "55px"
+            },
+            clipPath: {
+                'my-polygon': 'polygon(0px 0px, 100% 0%, 100% 100%, 0 300px)',
+            },
+        }
     },
-    textColor: {
-      primary: "#696969",
-      primaryHover: "#924dff",
-      blackColor: "#2f3045",
-    },
-    fontSize: {
-      primary: "16px",
-      secondary:"20px"
-    }
-  },
-  plugins: [],
+    plugins: [
+        require('tailwind-clip-path'),
+    ],
 }
