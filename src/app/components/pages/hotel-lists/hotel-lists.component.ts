41515f04c26467cf1e9af64bcf5ecab12d02fc97
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-02 15:44:47 +0100

    chore: cambio en las estructuras de las carpetas

--- src/app/components/pages/hotel-lists/hotel-lists.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-hotel-lists',
-  standalone: true,
-  imports: [],
-  templateUrl: './hotel-lists.component.html',
-
-})
-export class HotelListsComponent {}
