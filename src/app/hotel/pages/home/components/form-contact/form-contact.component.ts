b06003be8b7761d22a608c721261aab8682f23cb
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-03 19:20:58 +0100

    add: agrego formulario reactivo a componente y actualizo estilos en el formulario

--- src/app/hotel/pages/home/components/form-contact/form-contact.component.ts
+++ src/app/hotel/pages/home/components/form-contact/form-contact.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AppService } from '../../../../../service/app.service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-form-contact',
-  standalone: true,
-  imports: [],
-  templateUrl: './form-contact.component.html',
+    selector: 'app-form-contact',
+    standalone: true,
+    imports: [ReactiveFormsModule, FormsModule],
+    templateUrl: './form-contact.component.html',
 })
 export class FormContactComponent {
+    public appService = inject(AppService);
+    public form: FormGroup = inject(FormBuilder).group({
+        fullName: ['', [Validators.email, Validators.required]],
+        phoneNumber: ['', [Validators.required, Validators.minLength(3)]],
+        messageBody: ['', [Validators.required, Validators.minLength(3)]]
+    })
 
+    handleSubmitForm(): void {
+        this.appService.showMessagePage();
+    }
 }
