e823fc1d8a11739968d05a0d5cc6dcae07ab10d3
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-08 00:43:51 +0100

    refactor: eliminar contenido obsoleto y reorganizar importaciones en el componente de inicio

--- src/app/hotel/pages/home/home.component.ts
+++ src/app/hotel/pages/home/home.component.ts
@@ -8,12 +8,19 @@ import 'dayjs/locale/es';
 import { ListServicesComponent } from "./sections/list-services/list-services.component";
 import { SectionOneComponent } from "./sections/list-rooms/list-rooms.component";
 import { FormContactMapsComponent } from "./sections/formContact-maps/formContact-maps.component";
-import { FormSearchRoomComponent } from "../../components/formSearchRoom/formSearchRoom.component";
+import { FormSearchRoomComponent } from "../../../shared/formSearchRoom/formSearchRoom.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [RouterModule, ReactiveFormsModule, CommonModule, ListServicesComponent, SectionOneComponent, FormContactMapsComponent, FormSearchRoomComponent],
+    imports: [RouterModule,
+        ReactiveFormsModule,
+        CommonModule,
+        ListServicesComponent,
+        SectionOneComponent,
+        FormContactMapsComponent,
+        FormSearchRoomComponent
+    ],
     templateUrl: './home.component.html',
 })
 export default class HomeComponent {
