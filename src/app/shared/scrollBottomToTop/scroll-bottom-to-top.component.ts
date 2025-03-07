9d1e78f56e985e661c4f7077513291219e7fbbde
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-08 00:47:11 +0100

    feat: agregar componente para desplazarse al inicio de la página

---
+++ src/app/shared/scrollBottomToTop/scroll-bottom-to-top.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, HostListener, signal } from '@angular/core';
+
+@Component({
+    selector: 'app-scroll-bottom-to-top',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './scroll-bottom-to-top.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ScrollBottomToTopComponent {
+
+    public scrollPosition = signal<number>(0);
+    public showButton = computed(() => this.scrollPosition() > 400);
+
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+        this.scrollPosition.set(window.scrollY);
+    }
+
+    scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+}
