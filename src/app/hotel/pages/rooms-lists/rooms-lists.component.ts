4018217d84b64881f0fd995e1f4747bc33875867
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-08 00:45:51 +0100

    refactor: mejorar el diseño y la estructura del componente de listas de habitaciones y aplicando signal

--- src/app/hotel/pages/rooms-lists/rooms-lists.component.ts
+++ src/app/hotel/pages/rooms-lists/rooms-lists.component.ts
@@ -1,28 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { Hotel } from '../../interfaces/hotel.interface';
-import { HotelsService } from '../../services/hotels.service';
-import { Router } from '@angular/router';
-import { FormSearchRoomComponent } from "../../components/formSearchRoom/formSearchRoom.component";
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormSearchRoomComponent } from "../../../shared/formSearchRoom/formSearchRoom.component";
 import { FiltersComponent } from "../../components/filters/filters.component";
 import { AsideComponent } from "../../components/aside/aside.component";
+import { FormBuilder } from '@angular/forms';
+import { FormRoomsService } from '../../../shared/service/formRooms.service';
+
 @Component({
     selector: 'app-hotel-lists',
     standalone: true,
     imports: [CommonModule, FormSearchRoomComponent, FiltersComponent, AsideComponent],
     templateUrl: './rooms-lists.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class HotelListsComponent implements OnInit {
 
     public hotels: Hotel[] = [];
+    public formBuilder = inject(FormBuilder);
+    private _formRoomService = inject(FormRoomsService)
 
-    constructor(
-        private hotelService: HotelsService,
-        private router: Router
-    ) { }
-
+    private _router = inject(Router)
+    private _route = inject(ActivatedRoute);
     ngOnInit(): void {
-        this.hotels = this.hotelService.getHotelsData();
-    }
+        this._route.queryParams.subscribe(params => {
+            if (params['startDate'] || params['endDate'] || params['typeRoom']) {
+                this._formRoomService.getForm().patchValue({
+                    startDate: params['startDate'],
+                    endDate: params['endDate'],
+                    typeRoom: params['typeRoom']
+                });
+            }
+        });
+    };
+
 }
