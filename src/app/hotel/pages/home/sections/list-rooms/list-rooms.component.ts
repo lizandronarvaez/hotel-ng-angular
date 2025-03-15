2094cb3d201896a538f9506528b716934ebb0928
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-15 01:53:22 +0100

    refactor: eliminar componentes y archivos no utilizados

--- src/app/hotel/pages/home/sections/list-rooms/list-rooms.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-list-rooms',
-    standalone: true,
-    imports: [],
-    templateUrl: './list-rooms.component.html'
-})
-export class SectionOneComponent { }
