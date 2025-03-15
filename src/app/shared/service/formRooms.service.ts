22778f260c218cdf6dc96aeca0a3d2cf39948671
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-15 14:35:44 +0100

    refactor: eliminar el servicio de validación de fechas en el formulario de búsqueda de habitaciones

--- src/app/shared/service/formRooms.service.ts
+++ src/app/shared/service/formRooms.service.ts
@@ -1,18 +1,16 @@
 import { inject, Injectable } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ValidatorsService } from '../../core/service/validators.service';
 
 @Injectable({ providedIn: 'root' })
 export class FormRoomsService {
 
     private _formBuilder = inject(FormBuilder);
-    private _validatorsService = inject(ValidatorsService);
 
     public searchRooms: FormGroup = this._formBuilder.group({
         startDate: ['', Validators.required],
         endDate: ['', Validators.required],
         typeRoom: ['', Validators.required]
-    }, { validators: this._validatorsService.dateRangeValidator })
+    })
 
     getForm(): FormGroup {
         return this.searchRooms;
