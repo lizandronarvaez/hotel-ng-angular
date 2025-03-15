8604647f2d57b329db81ed778cc6b5fa0204da5f
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-15 01:54:14 +0100

    refactor: actualizar nombre del proyecto y ajustar dependencias en package.json, eliminar imágenes de fondo obsoletas en tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,31 +8,7 @@ export default {
         extend: {
             backgroundImage: {
                 "home-bg": "url('https://www.hotelgumontgdl.com/img/slider_horizontal/1.jpg')",
-                "bookingImage": "url('https://pro-static.h10hotels.com/gallery/CabeceraH10NOV2.jpg')",
                 "imageRooms": "url('https://media.licdn.com/dms/image/v2/C4E12AQGjw2-7pO1KWw/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1589378789505?e=2147483647&v=beta&t=qgbTQUvqhBXgvUdofx6zd__ldkeS4ESdtvtHdvmRhmQ')",
-                "reservation": "url('https://www.serinf.it/wp-content/uploads/2022/07/Cover-blog-check-in-check-out.jpg')"
-            },
-            backgroundColor: {
-                primaryBg: "#f3f7ff",
-                secondaryBg: "#924dff",
-                secondaryHoverBg: "#7440c7",
-                whiteBg: "#ffffff",
-                white2: "#f9f9f9"
-            },
-            textColor: {
-                primaryC: "#696969",
-                primaryHoverC: "#924dff",
-                blackColorC: "#2f3045",
-                whiteColorC: "#ffffff"
-            },
-            fontSize: {
-                primaryF: "16px",
-                secondaryF: "20px",
-                paragrapF: "24px",
-                titleF: "55px"
-            },
-            clipPath: {
-                'my-polygon': 'polygon(0px 0px, 100% 0%, 100% 100%, 0 300px)',
             },
             fontFamily: {
                 sans: ['Poppins', 'sans-serif'],
