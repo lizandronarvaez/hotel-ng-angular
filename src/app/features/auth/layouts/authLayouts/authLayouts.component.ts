e9ce883f7a30047987241dd467d66697ce835590
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-16 18:10:53 +0100

    refactor: eliminar la propiedad 'standalone' del componente AuthLayouts

--- src/app/features/auth/layouts/authLayouts/authLayouts.component.ts
+++ src/app/features/auth/layouts/authLayouts/authLayouts.component.ts
@@ -3,9 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-auth-layouts',
-    standalone: true,
     imports: [RouterOutlet],
     templateUrl: './authLayouts.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class AuthLayoutsComponent { }
