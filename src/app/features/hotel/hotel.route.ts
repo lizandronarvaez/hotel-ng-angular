ece976c8a1e0494feb0e200ed4391dff8232e3da
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-16 18:20:19 +0100

    refactor: eliminar ruta de inicio y agregar ruta para detalles de reserva

--- src/app/features/hotel/hotel.route.ts
+++ src/app/features/hotel/hotel.route.ts
@@ -3,11 +3,6 @@ import { Routes } from '@angular/router';
 export const HOTEL_ROUTES: Routes = [
 
     {
-        path: "",
-        title: 'Hotel AngularNG',
-        loadComponent: () => import('./pages/home/home.component')
-    },
-    {
         path: "lists-rooms",
         title: 'Lista de Habitaciones',
         loadComponent: () => import('./pages/rooms-lists/rooms-lists.component')
@@ -15,7 +10,11 @@ export const HOTEL_ROUTES: Routes = [
     {
         path: "find-booking",
         title: 'Mi reserva',
-        loadComponent: () => import('./pages/booking-room/booking-room.component')
+        loadComponent: () => import('./pages/booking-room/booking-room.component'),
+    },
+    {
+        path: "details-booking/:id",
+        loadComponent: () => import('./pages/booking-details/booking-details.component')
     },
     {
         path: "contact",
