ed5f1e627e345c883766877ba2093704814da6ca
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-16 18:18:25 +0100

    refactor: agregar ruta para el componente Home en la configuración de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
+import HomeComponent from './features/hotel/pages/home/home.component';
 export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
     {
         path: 'hotel-angular',
         loadChildren: () => import("./features/hotel/hotel.route").then(hotel => hotel.HOTEL_ROUTES)
@@ -7,11 +12,10 @@ export const routes: Routes = [
     {
         path: 'auth',
         loadChildren: () => import("./features/auth/auth.routes").then(auth => auth.AUTH_ROUTES)
-
     },
     {
         path: "**",
-        redirectTo: "hotel-angular",
+        redirectTo: "",
         pathMatch: 'full'
     }
 ];
