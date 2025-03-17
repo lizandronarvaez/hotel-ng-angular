891c6571a5a37b8e38691c576f09b14c6dba24bc
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-17 22:22:34 +0100

    feat: agregar propiedades de paginación a la interfaz ResponseRoom

--- src/app/features/hotel/interfaces/rooms/responseRooms.interface.ts
+++ src/app/features/hotel/interfaces/rooms/responseRooms.interface.ts
@@ -1,7 +1,11 @@
 import { Room } from "./room.interface";
 
 export interface ResponseRoom {
-  statusCode: number;
-  message:    string;
-  roomList:   Room[];
+    statusCode: number;
+    message: string;
+    roomList: Room[];
+    pageNumber: number,
+    pageSize: number,
+    totalElements: number,
+    totalPages: number
 }
