ec4c18efe94c0f808b7ba8a6725b993472f9ecc3
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-17 22:22:25 +0100

    feat: mejorar formulario de contacto con validaciones y mensajes de error

--- src/app/features/hotel/components/form-contact/form-contact.component.ts
+++ src/app/features/hotel/components/form-contact/form-contact.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from '@angular/core';
-
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
 import { AppService } from '../../../../core/service/app.service';
+import { ValidatorsService } from '../../../../core/service/validators.service';
 
 
 @Component({
@@ -10,14 +11,25 @@ import { AppService } from '../../../../core/service/app.service';
     templateUrl: './form-contact.component.html'
 })
 export class FormContactComponent {
+
+    public validatorsForms = inject(ValidatorsService);
     public appService = inject(AppService);
+
     public form: FormGroup = inject(FormBuilder).group({
-        fullName: ['', [Validators.email, Validators.required]],
-        phoneNumber: ['', [Validators.required, Validators.minLength(3)]],
-        messageBody: ['', [Validators.required, Validators.minLength(3)]]
+        fullName: ['', Validators.required],
+        email: ['', [Validators.email, Validators.required]],
+        phoneNumber: [''],
+        message: ['', [Validators.required, Validators.minLength(10)]]
     })
 
     handleSubmitForm(): void {
+        if (this.form.invalid) {
+            this.form.markAllAsTouched();
+            return;
+        }
+
+        // TODO: REALIZAR ENVIO DE MENSAJE DE CONTACTO AL BACKEND
         this.appService.showMessagePage();
     }
+
 }
