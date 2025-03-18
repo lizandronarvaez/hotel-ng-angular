3de84190d060bc009bc8d019099d2751cf4fa978
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-03-18 21:27:43 +0100

    feat: agregar componente de cargador esquelético para mejorar la experiencia de carga

---
+++ src/app/features/hotel/components/skeleton-loader/squeleton-loader.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'app-skeleton-loader',
+    imports: [],
+    templateUrl: './skeleton-loader.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SkeletonLoaderComponent { }
