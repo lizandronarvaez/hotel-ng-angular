9d1489fc48152bda7b0c3146efab6f09f7618c20
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-04-22 23:03:34 +0200

    feat: corregir nombre de campo de teléfono en el formulario de contacto

--- src/app/features/hotel/components/form-contact/form-contact.component.ts
+++ src/app/features/hotel/components/form-contact/form-contact.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 
 import { AppService } from '../../../../core/service/app.service';
 import { ValidatorsService } from '../../../../core/service/validators.service';
+import { environments } from '../../../../../environments/environments';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -14,22 +16,35 @@ export class FormContactComponent {
 
     public validatorsForms = inject(ValidatorsService);
     public appService = inject(AppService);
+    private backendUrl = environments.backendApiUrl;
+    private httpClient = inject(HttpClient);
 
-    public form: FormGroup = inject(FormBuilder).group({
-        fullName: ['', Validators.required],
-        email: ['', [Validators.email, Validators.required]],
-        phoneNumber: [''],
-        message: ['', [Validators.required, Validators.minLength(10)]]
-    })
+    public form: FormGroup = inject(FormBuilder)
+        .group({
+            fullName: ['', Validators.required],
+            email: ['', [Validators.email, Validators.required]],
+            numberPhone: [''],
+            message: ['', [Validators.required, Validators.minLength(10)]]
+        })
 
     handleSubmitForm(): void {
         if (this.form.invalid) {
             this.form.markAllAsTouched();
             return;
         }
+        this.httpClient.post(`${this.backendUrl}/users/form-contact`, this.form.value).subscribe({
+            next: (data) => {
+                console.log('Respuesta del backend:', data);
 
+            },
+            error: (error) => {
+                console.error('Error al enviar el formulario:', error);
+            }
+        });
+        
+        this.form.reset()
         // TODO: REALIZAR ENVIO DE MENSAJE DE CONTACTO AL BACKEND
-        this.appService.showMessagePage();
+        // this.appService.showMessagePage();
     }
 
 }
