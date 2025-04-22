5dc1229e3ca290495357ab58b5dca618eae13184
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-04-22 23:02:19 +0200

    chore: eliminar componente de administración de usuarios

--- src/app/features/auth/pages/admin/admin.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-    selector: 'app-admin',
-    imports: [],
-    templateUrl: './admin.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
-})
-export default class AdminComponent { }
