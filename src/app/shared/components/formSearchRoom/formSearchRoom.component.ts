31c7748f56db4bded52338dadee9bb1411b31f32
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-04-22 23:04:55 +0200

    feat: agregar emisión de búsqueda de habitaciones y mejorar la suscripción a tipos de habitaciones

--- src/app/shared/components/formSearchRoom/formSearchRoom.component.ts
+++ src/app/shared/components/formSearchRoom/formSearchRoom.component.ts
@@ -1,12 +1,13 @@
-import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { ValidatorsService } from '../../../core/service/validators.service';
 import { FormRoomsService } from '../../service/formRooms.service';
 import { RoomsService } from '../../../features/hotel/services/rooms.service';
+import { SearchRoom } from '../../../features/hotel/interfaces/rooms/searchRooms.interface';
 
 @Component({
     selector: 'app-form-search-room',
@@ -21,18 +22,21 @@ export class FormSearchRoomComponent implements OnInit, OnDestroy {
     public validatorService = inject(ValidatorsService);
     public formRoomService = inject(FormRoomsService)
     public roomTypes = signal<string[]>([]);
+    public searchRooms = output<SearchRoom>();
 
+    private activateRouter = inject(ActivatedRoute)
     private router = inject(Router)
     private hotelService = inject(RoomsService);
     private subscription: Subscription = new Subscription();
 
 
     ngOnInit(): void {
-        const suscriptionTypeRooms = this.hotelService.getTypesRooms().subscribe({
-            next: (data) => this.roomTypes.set(data),
-            error: () => this.roomTypes.set([])
-        })
-        this.subscription.add(suscriptionTypeRooms);
+        this.subscription = this.hotelService.getTypesRooms()
+            .subscribe({
+                next: (data) => this.roomTypes.set(data),
+                error: () => this.roomTypes.set([])
+            })
+
     }
 
 
@@ -41,9 +45,11 @@ export class FormSearchRoomComponent implements OnInit, OnDestroy {
             this.formRoomService.getForm().markAllAsTouched();
             return;
         }
+
         this.router.navigate(['/hotel-angular/lists-rooms'], {
             queryParams: this.formRoomService.getForm().value
         });
+        this.searchRooms.emit(this.formRoomService.getForm().value)
     }
 
     ngOnDestroy(): void {
