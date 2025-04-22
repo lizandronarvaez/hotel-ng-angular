3d9d044237cf8fb26c41695eb29f8b02c6ea3ca3
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-04-22 22:58:14 +0200

    feat: agregar componente de administración de reservas de hotel

---
+++ src/app/features/admin/pages/admin-hotel-bookings/administration-hotel-bookings.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administration-hotel-bookings',
+  imports: [],
+  templateUrl:'./administration-hotel-bookings.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class AdministrationHotelBookingsComponent { }
