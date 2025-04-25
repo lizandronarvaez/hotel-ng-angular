7ab8a03bacf6856a06e98762412e45bdc32668b2
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-04-25 22:09:56 +0200

    fix: corregir importación y formato en el componente de reserva

--- src/app/features/hotel/pages/booking-room/booking-room.component.ts
+++ src/app/features/hotel/pages/booking-room/booking-room.component.ts
@@ -3,9 +3,10 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { AppService } from '../../../../core/service/app.service';
-import { BookingService } from '../../services/booking.service';
+
 import { FormRoomsService } from '../../../../shared/service/formRooms.service';
 import { ValidatorsService } from '../../../../core/service/validators.service';
+import { BookingService } from '../../../../core/service/booking.service';
 
 
 @Component({
@@ -14,18 +15,17 @@ import { ValidatorsService } from '../../../../core/service/validators.service';
     templateUrl: './booking-room.component.html'
 })
 export default class ReservationComponent {
-
-    private router = inject(Router);
-
-    public appService = inject(AppService);
-    public bookingService = inject(BookingService);
-    public formBuilder = inject(FormBuilder);
+    
+  public appService = inject(AppService);
+  public bookingService = inject(BookingService);
+  public formBuilder = inject(FormBuilder);
     public formRoomService = inject(FormRoomsService);
     public validatorService = inject(ValidatorsService);
     public form: FormGroup = inject(FormBuilder).group({
-        bookingCode: ['', [Validators.required]],
+      bookingCode: ['', [Validators.required]],
     })
 
+    private router = inject(Router);
     handleSubmitBooking(): void {
         if (this.form.invalid) {
             this.form.markAllAsTouched();
@@ -33,7 +33,7 @@ export default class ReservationComponent {
         }
 
         const bookingCode = this.form.value.bookingCode;
-        this.router.navigateByUrl(`/hotel-angular/details-booking/${bookingCode}`);
+        this.router.navigateByUrl(`/booking/details-booking/${bookingCode}`);
     }
 
 }
