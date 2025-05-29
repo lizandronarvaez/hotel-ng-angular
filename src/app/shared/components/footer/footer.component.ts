ed0a34f23c1faa999f9001f6006d48f749625adf
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-05-29 19:42:24 +0200

    fix: eliminar importación innecesaria de CommonModule en el componente de pie de página

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-footer',
-    imports: [CommonModule, RouterLink],
+    imports: [RouterLink],
     templateUrl: './footer.component.html'
 })
 export class FooterComponent { }
