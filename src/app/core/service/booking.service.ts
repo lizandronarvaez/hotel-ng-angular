64c951a305f81e17c699042351ce79b2ac8fa0a1
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2025-06-02 18:45:07 +0200

    fix: corregir la indentación y mejorar la estructura del servicio de reservas

--- src/app/core/service/booking.service.ts
+++ src/app/core/service/booking.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -6,16 +7,39 @@ import { ResponseBooking } from '../../features/hotel/interfaces/responseBooking
 
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class BookingService {
 
-    private backendUrl = environments.backendApiUrl;
-    private httpClient = inject(HttpClient);
+  private backendUrl = environments.backendApiUrl;
+  private httpClient = inject(HttpClient);
 
-    // Obtener  reserva por codigo de reserva
-    getBookingByBookingCode(bookingCode: string): Observable<ResponseBooking> {
-        return this.httpClient.get<ResponseBooking>(`${this.backendUrl}/bookings/get-by-booking-code/${bookingCode}`,);
-    }
+  // Crear un nueva reserva
+  createBooking(idRoom: any, room: any, client: any): Observable<any> {
+    const bookingData = { room, client };
+    console.log(bookingData);
+    return this.httpClient.post<boolean>(`${this.backendUrl}/bookings/new-reservation/book-room`, bookingData, {
+      params: { idRoom }
+    });
+  }
+
+  // eliminar reserva
+  deleteBooking(booking_id: number): void {
+    this.httpClient.delete(`${this.backendUrl}/bookings/cancel-booking/${booking_id}`,);
+  }
+  // Obtener  reserva por codigo de reserva
+  getBookingByBookingCode(bookingCode: string): Observable<ResponseBooking> {
+    return this.httpClient.get<ResponseBooking>(`${this.backendUrl}/bookings/get-by-booking-code/${bookingCode}`,);
+  }
+
+  // Obtener todas las habtaciones(solo admin)
+  getAllBookings(): Observable<ResponseBooking> {
+    return this.httpClient.get<ResponseBooking>(`${this.backendUrl}/bookings/all`);
+  }
+
+  // Obtiene las reservas de un usuario(solo admin)
+  getUserBookings(idUser: string): void {
+    this.httpClient.get(`${this.backendUrl}/users/get-user-bookings/${idUser}`)
+  }
 
 }
